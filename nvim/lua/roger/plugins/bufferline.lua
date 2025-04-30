fcc0c1d8f9b5a51a2e89d68277c129dfc1ced3f3
Roger Fan <sweric2u@gmail.com>
2025-04-30 16:56:18 +0800

    feat: don't show "neo-tree filesystem" on the tab but actual main page

--- nvim/lua/roger/plugins/bufferline.lua
+++ nvim/lua/roger/plugins/bufferline.lua
@@ -8,6 +8,17 @@ return {
       -- use ":h bufferline-configuration" to see all options
       options = {
         mode = "tabs",
+        name_formatter = function(tab)
+          -- 1. 找出 tab 內第一個「不是 neo-tree」的 buffer
+          for _, bufnr in ipairs(tab.buffers) do
+            if vim.bo[bufnr].filetype ~= "neo-tree" then
+              -- 用檔名 (t) 或完整路徑 (.) 都可
+              return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":t")
+            end
+          end
+          -- 2. 如果整個 tab 都只有 neo-tree，就回傳固定字串
+          return "File Explorer"
+        end,
         max_name_length = 30,
         max_prefix_length = 20,
         tab_size = 18,
@@ -20,9 +31,9 @@ return {
             text = "File Explorer",
             text_align = "center",
             separator = true,
-          }
+          },
         },
       },
     })
-  end
+  end,
 }
