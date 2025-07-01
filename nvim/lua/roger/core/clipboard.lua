8d44f6baa4a2a2f90e4d6de03b694b97c526c78f
Roger Fan <sweric2u@gmail.com>
2025-07-01 15:59:33 +0800

    fix: linux clipboard through SSH

---
+++ nvim/lua/roger/core/clipboard.lua
@@ -0,0 +1,14 @@
+-- only set up this if we are using SSH
+if vim.env.SSH_CLIENT then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
--- nvim/lua/roger/core/init.lua
+++ nvim/lua/roger/core/init.lua
@@ -1,3 +1,4 @@
 require("roger.core.colors")
 require("roger.core.options")
 require("roger.core.keymaps")
+require("roger.core.clipboard")
