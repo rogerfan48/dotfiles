ed04e5d0d3ed9e32bfe7e58d2bd3d2b2e4ba87e5
Roger Fan <sweric2u@gmail.com>
2025-07-28 14:04:52 +0800

    fix: Linux lazygit installation

--- initialization.sh
+++ initialization.sh
@@ -337,8 +337,9 @@ elif [[ "$OS" == "Linux" ]]; then
 
     echo "### Installing lazygit from binary..."
     if ! command -v lazygit >/dev/null 2>&1; then
-        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
-        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+        # LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+        LAZYGIT_VERSION=0.45.2
+        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
         tar xf lazygit.tar.gz lazygit
         sudo install lazygit /usr/local/bin
         rm lazygit.tar.gz lazygit
